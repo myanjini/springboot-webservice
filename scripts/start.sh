04967748b235b4a19d6b86f995563123f13640a1
user <user@host.docker.internal>
2020-05-11 15:57:24 +0900

    .

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,6 +14,8 @@ cp $REPOSITORY/zip/*.war $REPOSITORY/
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep war | tail -n 1)
 IDLE_PROFILE=$(find_idle_profile)
 
+chmod +x $JAR_NAME
+
 echo "> 새 애플리케이션($JAR_NAME) 배포"
 nohup java -jar \
 	-Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE \
